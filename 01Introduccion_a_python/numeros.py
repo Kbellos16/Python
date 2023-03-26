10d20a0fd53f0ed1ad56d707f5c7d9dba6171bbb
delgadocris <37419901+delgadocris@users.noreply.github.com>
2023-03-26 16:54:20 -0400

    Add comment in numero file

--- 01Introduccion_a_python/numeros.py
+++ 01Introduccion_a_python/numeros.py
@@ -1,8 +1,10 @@
-num1= 20
+# Trabajar números con python
+num1=20
 num2=22.4
 print(int(num2))
 print(type(num2))
 print(float(num1))
 print(type(num1))
-'''asi se puede
-coemtar un codigo de varias lineas y ademas tambien se puede convertir un float a int'''
+'''Este es un ejemplo de como se puede comentar un
+código de varias líneas y además también se puede
+convertir un float a int'''
